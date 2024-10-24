991546be8e5be33c5b1d4a0190fd2c2652481831
Charles0304 <119788062+Charles0304@users.noreply.github.com>
2024-10-24 16:06:10 +0900

    [level 1] Title: 추억 점수, Time: 2.40 ms, Memory: 97.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        Map<String,Integer> map = new HashMap<String,Integer>();
+        int total = 0;
+        for(int i=0;i<name.length;i++){
+            map.put(name[i],yearning[i]);
+        }
+        for(int i=0;i<photo.length;i++){
+            for(int j=0;j<photo[i].length;j++){
+                if(map.containsKey(photo[i][j]))
+                    total+=map.get(photo[i][j]);
+            }
+            answer[i]=total;
+            total=0;
+            
+        }
+        return answer;
+    }
+}
