c00225330f689512efe261ec347f797db7bd8e9e
Charles0304 <119788062+Charles0304@users.noreply.github.com>
2024-11-22 14:59:00 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 70.9 MB -BaekjoonHub

--- Java/프로그래머스/2/42842. 카펫/카펫.java
+++ Java/프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int h = 0;
-        for(int i=1;i<=Math.sqrt(yellow);i++) {
-        	if(yellow%i==0) {
-        		h=yellow/i;
-        		if(i*2+h*2+4==brown) {
-        			answer[1]=i+2;
-        			answer[0]=h+2;
-        			break;
-        		}
-        	}
+        int[] answer = {};
+        int h=0,w=0;
+        for(int i=1;i<=yellow;i++){
+            if(yellow%i==0){
+                h=i;
+                w=yellow/h;
+                if(2*h+2*w+4==brown)
+                    break;
+            }
         }
-        return answer;
+        return new int[]{w+2,h+2};
     }
 }
