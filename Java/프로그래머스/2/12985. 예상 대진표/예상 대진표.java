080ca0c02559767ac1844ff6a7f0e6b65084ce78
Charles0304 <119788062+Charles0304@users.noreply.github.com>
2024-11-26 13:33:36 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 73.9 MB -BaekjoonHub

--- Java/프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ Java/프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -3,22 +3,15 @@ class Solution
     public int solution(int n, int a, int b)
     {
         int answer = 0;
-        if(a>b){
-            int tmp = b;
-            b=a;
-            a=tmp;
-        }
-        while(true){
-        	answer++;
-            if(b%2==0&&b-a==1)
-                break;
-            if(a%2==1)
-                a+=1;
-            if(b%2==1)
-                b+=1;
-            a=a/2;
-            b=b/2;
-           
+
+        while(a!=b){
+            if(a%2!=0)
+                a=a+1;
+            if(b%2!=0)
+                b=b+1;
+            a/=2;
+            b/=2;
+            answer++;
         }
 
         return answer;
