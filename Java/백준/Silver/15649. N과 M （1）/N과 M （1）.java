dcd520f79fd641e756e9f73ac20b37c1af7a37a3
Charles0304 <119788062+Charles0304@users.noreply.github.com>
2025-02-18 14:50:10 +0900

    [Silver III] Title: N과 M (1), Time: 420 ms, Memory: 32900 KB -BaekjoonHub

---
+++ Java/백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+        Stack<Integer> stack = new Stack<>();
+        int i=1;
+        stack.push(i);
+        while(true){
+            if (stack.size() == m) {
+                for (int num : stack) {
+                    bw.write(num + " ");
+                }
+                bw.write("\n");
+                i = stack.pop() + 1;
+                continue;
+            }
+            if (!stack.isEmpty()&& stack.contains(i)) {
+                i++;
+                continue;
+            }
+            if (i > n) {
+                if(!stack.isEmpty()) {
+                    i = stack.pop() + 1;
+                    continue;
+                }
+                break;
+            }
+            if (stack.size() < m) {
+                stack.push(i);
+                i=1;
+                continue;
+            }
+
+
+            break;
+        }
+        bw.flush();
+
+    }
+}
